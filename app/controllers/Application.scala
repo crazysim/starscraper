aa793212df0b1142f4d61ccf25ca1102b5bfd875
Nelson Chen <crazysim@gmail.com>
2012-09-10 16:47:00 -0700

    try ticket

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,39 @@
 package controllers
 
 import play.api._
+import libs.concurrent.Akka
 import play.api.mvc._
+
+import play.api.Play.current
 
 import org.openqa.selenium._
 import org.openqa.selenium.htmlunit._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    val driver = new HtmlUnitDriver()
-    driver.get("http://resnetservice.housing.ucsb.edu/")
-    Ok(driver.getPageSource)
+    val promiseOfSource = Akka.future {
+      val driver = new HtmlUnitDriver()
+      driver.get("http://slickdeals.net/")
+      driver.getPageSource
+    }
+    AsyncResult {
+      promiseOfSource.map(s => Ok(s))
+    }
   }
-  
+
+  def ticket(id: String) = Action {
+    val promiseOfSource = Akka.future {
+      val driver = new HtmlUnitDriver()
+      driver.get("http://resnetservice.housing.ucsb.edu/")
+      val login_source = driver.getPageSource
+      driver.close()
+      login_source
+    }
+
+    AsyncResult {
+      promiseOfSource.map(s => Ok(s))
+    }
+  }
+
 }
