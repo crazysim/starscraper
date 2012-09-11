f13ff101ef50873105df6e885f1e52e3cf8dc689
Nelson Chen <crazysim@gmail.com>
2012-09-10 17:33:11 -0700

    rough pull

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -32,7 +32,7 @@ object Application extends Controller {
     }
   }
 
-  def getTicket(id: String): String = {
+  def getTicket(id: String = "5432"): String = {
     val driver = new HtmlUnitDriver()
     driver.setJavascriptEnabled(true)
     driver.get("http://resnetservice.housing.ucsb.edu/")
@@ -41,8 +41,12 @@ object Application extends Controller {
     driver.findElementByName("txtUserName").sendKeys(username)
     driver.findElementByName("txtPassword").sendKeys(password)
     driver.findElementByName("btnLogin").click()
-    val src = driver.getPageSource
+    val landing_src = driver.getPageSource
+    driver.get("http://resnetservice.housing.ucsb.edu/hsPages/RB_RequestTemplate.aspx?requestId=" + id + "&ActiveTabIndex=0&TabTobeLoaded=tabTransactions&OrderBy=undefined&MemoId=undefined&Preview=0")
+    val request_src = driver.getPageSource
+    driver.get("http://resnetservice.housing.ucsb.edu/hsPages/RB_UDFTemplate.aspx?ObjectId=" + id + "&ActiveTabIndex=0&TabTobeLoaded=tabUDFs ")
+    val udf_src = driver.getPageSource
     driver.close()
-    src
+    landing_src + request_src + udf_src
   }
 }
