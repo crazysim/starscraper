342d6311b8e4d1835f7ab9aaae59356e325c1dd7
Nelson Chen <crazysim@gmail.com>
2012-09-11 22:03:33 -0700

    rough model

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,8 @@ import play.api.Play.current
 import org.openqa.selenium._
 import org.openqa.selenium.htmlunit._
 
+import models.HelpSTARScrape.getTicket
+
 object Application extends Controller {
 
   def index = Action {
@@ -32,21 +34,5 @@ object Application extends Controller {
     }
   }
 
-  def getTicket(id: String = "5432"): String = {
-    val driver = new HtmlUnitDriver()
-    driver.setJavascriptEnabled(true)
-    driver.get("http://resnetservice.housing.ucsb.edu/")
-    val username = current.configuration.getString("helpstar.username").getOrElse("None")
-    val password = current.configuration.getString("helpstar.password").getOrElse("None")
-    driver.findElementByName("txtUserName").sendKeys(username)
-    driver.findElementByName("txtPassword").sendKeys(password)
-    driver.findElementByName("btnLogin").click()
-    val landing_src = driver.getPageSource
-    driver.get("http://resnetservice.housing.ucsb.edu/hsPages/RB_RequestTemplate.aspx?requestId=" + id + "&ActiveTabIndex=0&TabTobeLoaded=tabTransactions&OrderBy=undefined&MemoId=undefined&Preview=0")
-    val request_src = driver.getPageSource
-    driver.get("http://resnetservice.housing.ucsb.edu/hsPages/RB_UDFTemplate.aspx?ObjectId=" + id + "&ActiveTabIndex=0&TabTobeLoaded=tabUDFs ")
-    val udf_src = driver.getPageSource
-    driver.close()
-    request_src + udf_src
-  }
+
 }
