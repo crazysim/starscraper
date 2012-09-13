8c5b45732b73257d711f39d2b5726e06fa9016ac
Nelson Chen <crazysim@gmail.com>
2012-09-13 15:59:05 -0700

    test properties first

--- test/models/HelpSTARTest.scala
+++ test/models/HelpSTARTest.scala
@@ -9,21 +9,15 @@ import scala.xml._
 
 class HelpSTARTest extends Specification {
 
-  "The 'Hello world' string" should {
-    "contain 11 characters" in {
-      "Hello world" must have size (11)
+  "File Parsing" should {
+    "parse properties" in {
+      "Hello world" must have size(11)
     }
-    "start with 'Hello'" in {
-      "Hello world" must startWith("Hello")
-    }
-    "end with 'world'" in {
-      "Hello world" must endWith("world")
-    }
-  }
-
-//  "File loading" should {
-//    "Load Nasty XML" in {
-//      readDirtyHTMLInputSteam(getClass.getResourceAsStream("details.html")) must \\(<html/>)
+//    "parse transactions" in {
+//      "Hello world" must startWith("Hello")
 //    }
-//  }
+//    "parse UDFs" in {
+//      "Hello world" must endWith("world")
+//    }
+  }
 }
