98ac41d320430bcc7deeb18ebadfdafd852598c2
Nelson Chen <crazysim@gmail.com>
2012-09-13 22:23:45 -0700

    more testings

--- test/models/HelpSTARTest.scala
+++ test/models/HelpSTARTest.scala
@@ -11,22 +11,19 @@ import scala.xml._
 class HelpSTARTest extends Specification {
 
   "Detail Parsing" should {
+    val properties = HelpSTAR.parseDetails(details_HTML)
     "parse example details to not be empty" in {
-      val properties = HelpSTAR.parseDetails(details_HTML)
       properties.size must_!= 0
     }
     "parse example title is Cannot Register with Given Perm & PW" in {
-      val properties = HelpSTAR.parseDetails(details_HTML)
       properties("Title") mustEqual ("Cannot Register with Given Perm & PW")
     }
 
     "parse example number to 5432" in {
-      val properties = HelpSTAR.parseDetails(details_HTML)
       properties("Number") mustEqual ("5432")
     }
 
     "parse example to a status that has Closed" in {
-      val properties = HelpSTAR.parseDetails(details_HTML)
       properties("Status") must contain("Closed")
     }
 
@@ -40,6 +37,15 @@ class HelpSTARTest extends Specification {
 
   def details_HTML = {
     val is = getClass.getResourceAsStream("details.html")
+    HelpSTAR.readDirtyHTMLInputSteam(is)
+  }
+
+  "Transaction Parsing" should {
+    success
+  }
+
+  def transactions_HTML = {
+    val is = getClass.getResourceAsStream("transactions.html")
     HelpSTAR.readDirtyHTMLInputSteam(is)
   }
 }
