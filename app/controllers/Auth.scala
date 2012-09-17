2e2ebfb68ae1ba33f94f1f7bc4720e76988609bb
Nelson Chen <crazysim@gmail.com>
2012-09-17 15:12:24 -0700

    login implemented

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -1,11 +1,23 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, AsyncResult, Controller}
+import play.api.libs.concurrent.{Redeemed, Thrown}
+import play.api.libs.openid.OpenID
 
 
 object Auth extends Controller {
 
-  def login = TODO
+  def login = Action { implicit r =>
+    val openid = "https://www.google.com/accounts/o8/id"
+    AsyncResult(OpenID.redirectURL(openid,
+      routes.Auth.openIDCallBack().absoluteURL(),
+      Seq("email" -> "http://schema.openid.net/contact/email")
+    )
+      .extend( _.value match {
+      case Redeemed(url) => Redirect(url)
+      case Thrown(t) => Redirect(routes.Auth.login())
+    }))
+  }
 
   def authenticate = TODO
 
