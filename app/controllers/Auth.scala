68ad6b6a5fd31340127c42a2a800055b5bf2fd7f
Nelson Chen <crazysim@gmail.com>
2012-09-17 16:35:07 -0700

    reformat

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -7,38 +7,40 @@ import play.api.libs.openid.OpenID
 
 object Auth extends Controller {
 
-  def login = Action { implicit r =>
-    val openid = "https://www.google.com/accounts/o8/id"
-    AsyncResult(OpenID.redirectURL(openid,
-      routes.Auth.openIDCallBack().absoluteURL(),
-      Seq("email" -> "http://schema.openid.net/contact/email")
-    )
-      .extend( _.value match {
-      case Redeemed(url) => Redirect(url)
-      case Thrown(t) => Redirect(routes.Auth.login())
-    }))
+  def login = Action {
+    implicit r =>
+      val openid = "https://www.google.com/accounts/o8/id"
+      AsyncResult(OpenID.redirectURL(openid,
+        routes.Auth.openIDCallBack().absoluteURL(),
+        Seq("email" -> "http://schema.openid.net/contact/email")
+      )
+        .extend(_.value match {
+        case Redeemed(url) => Redirect(url)
+        case Thrown(t) => Redirect(routes.Auth.login())
+      }))
   }
 
   def authenticate = TODO
 
-  def openIDCallBack = Action { implicit request =>
-    AsyncResult(
-      OpenID.verifiedId.extend( _.value match {
-        case Redeemed(info) => {
-          info.attributes.get("email") match {
-            case None => Redirect(routes.Auth.login())
-            case Some(email) => Redirect(routes.Application.index()).withSession(Security.username -> email)
+  def openIDCallBack = Action {
+    implicit request =>
+      AsyncResult(
+        OpenID.verifiedId.extend(_.value match {
+          case Redeemed(info) => {
+            info.attributes.get("email") match {
+              case None => Redirect(routes.Auth.login())
+              case Some(email) => Redirect(routes.Application.index()).withSession(Security.username -> email)
+            }
           }
-        }
-        case Thrown(t) => {
-          // Here you should look at the error, and give feedback to the user
-          Redirect(routes.Auth.login())
-        }
-      })
-    )
+          case Thrown(t) => {
+            // Here you should look at the error, and give feedback to the user
+            Redirect(routes.Auth.login())
+          }
+        })
+      )
   }
 
-  def logout = Action{
+  def logout = Action {
     Redirect(routes.Auth.login()).withNewSession
   }
 
