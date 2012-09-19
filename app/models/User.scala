15b64cdba8825816c72087210edc4c7721d0a009
Nelson Chen <crazysim@gmail.com>
2012-09-18 22:42:48 -0700

    very rough

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,7 +1,16 @@
 package models
 
+import play.api.db.DB
+import anorm._
+import anorm.SqlParser._
+
 object User {
   def isAuthorized(email: String): Boolean = {
-    false
+    DB.withConnection(implicit c =>
+      SQL("SELECT count(*) FROM users WHERE email = '{email}'").on("email"->email).as(scalar[Int].single) match {
+        case 1 => true
+        case _ => false
+      }
+    )
   }
 }
